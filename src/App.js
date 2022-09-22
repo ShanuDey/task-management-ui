70ec0a379d169888dfc2013155c1a9f244d8fe7e
ShanuDey <sd4shanudey@gmail.com>
2022-09-22 18:42:32 +0530

    implemented routing with react router dom
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./page/Dashboard";
+import Login from "./page/Login";
+import Register from "./page/Register";
+import Welcome from "./page/Welcome";
+
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Routes>
+      <Route path="/" element={<Welcome/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
   );
 }
 
