45f520aeb9279817afc0a48390202d319a7442c4
ShanuDey <sd4shanudey@gmail.com>
2022-09-23 18:23:17 +0530

    design welcome page
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/page/Welcome.jsx
+++ src/page/Welcome.jsx
@@ -1,7 +1,17 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import Layout from "../component/Layout";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <Layout>
+      <Box marginTop="20vh" textAlign="center">
+        <Typography variant="h1" color="#264653" marginBottom="20px">Task Manager</Typography>
+        <Button variant="outlined">GET STARTED</Button>
+      </Box>
+    </Layout>
+  );
 };
 
 export default Welcome;
