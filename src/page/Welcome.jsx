2bae2a7d678cbdd1bc67bbc08237a0517a2460e5
ShanuDey <sd4shanudey@gmail.com>
2022-09-25 07:50:44 +0530

    nagigate to dashboard on click of get started button
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/page/Welcome.jsx
+++ src/page/Welcome.jsx
@@ -1,16 +1,22 @@
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Layout from "../component/Layout";
 
 const Welcome = () => {
+  const navigate = useNavigate();
+  const handleGetStarted = () => {
+    navigate('/dashboard');
+  }
+
   return (
     <Layout>
       <Box marginTop="20vh" textAlign="center">
         <Typography variant="h1" color="#264653" marginBottom="20px">
           Task Manager
         </Typography>
-        <Button variant="outlined">GET STARTED</Button>
+        <Button variant="outlined" onClick={handleGetStarted}>GET STARTED</Button>
       </Box>
     </Layout>
   );
