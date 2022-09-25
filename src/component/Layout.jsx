d0ac74f34594c662aae9c1cb29472fb2b07604c3
ShanuDey <sd4shanudey@gmail.com>
2022-09-25 07:02:22 +0530

    update layout
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./Header";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Footer from "./Footer";
+
+const theme = createTheme();
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
-      {children}
-    </>
+      
+        {children}
+      
+      <Footer />
+    </ThemeProvider>
   );
 };
 
