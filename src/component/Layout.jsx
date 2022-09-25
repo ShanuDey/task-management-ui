bdf87fc90bd50efd4735b622740502a50206a86d
ShanuDey <sd4shanudey@gmail.com>
2022-09-25 07:11:04 +0530

    update layout with container
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Footer from "./Footer";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 
 const theme = createTheme();
 
@@ -9,9 +11,10 @@ const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
         {children}
-      
+      </Container>
       <Footer />
     </ThemeProvider>
   );
