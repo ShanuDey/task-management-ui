b82856b56c84f5587daba3ee3710ddeabb6e2695
ShanuDey <sd4shanudey@gmail.com>
2022-09-27 17:29:03 +0530

    added recoil for state management
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
 );
