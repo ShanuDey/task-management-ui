ca595db5ca3ce816f7c7a527dc9c4fca7c883676
ShanuDey <sd4shanudey@gmail.com>
2022-09-27 08:08:15 +0530

    added react-toastify
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -4,6 +4,8 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Footer from "./Footer";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const theme = createTheme();
 
@@ -16,6 +18,17 @@ const Layout = ({ children }) => {
         {children}
       </Container>
       <Footer />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </ThemeProvider>
   );
 };
