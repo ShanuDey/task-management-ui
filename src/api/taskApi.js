46d40bccd0c335fd66164a4574f9ba184d0f2428
ShanuDey <sd4shanudey@gmail.com>
2022-10-02 19:15:32 +0530

    implement update task in task api
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -18,6 +18,26 @@ class TaskApi {
       throw new Error("Something went wrong !!");
     }
   }
+
+  async updateTask(tokenHeader, taskID, jsonData) {
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API_BASE_URL + `/task/${taskID}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-token": tokenHeader,
+          },
+          body: JSON.stringify(jsonData),
+        }
+      );
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      throw new Error("Something went wrong !!");
+    }
+  }
 }
 
 export default new TaskApi();
