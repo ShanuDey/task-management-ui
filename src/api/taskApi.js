930b757dac381be255890b4c421097e9a03cbf2f
ShanuDey <sd4shanudey@gmail.com>
2022-10-02 20:34:54 +0530

    implemented get tasks in task api
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -38,6 +38,25 @@ class TaskApi {
       throw new Error("Something went wrong !!");
     }
   }
+
+  async getTasks(tokenHeader) {
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API_BASE_URL + "/task",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-token": tokenHeader,
+          },
+        }
+      );
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      throw new Error("Something went wrong !!");
+    }
+  }
 }
 
 export default new TaskApi();
