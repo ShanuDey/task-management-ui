78c18a6547f2608e956a7654f80cff1601b8191d
ShanuDey <sd4shanudey@gmail.com>
2022-10-05 09:31:26 +0530

    Add loading UI in dashboard resolved #4
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Layout from "../component/Layout";
 import { useRecoilState } from "recoil";
@@ -10,10 +10,12 @@ import AddTask from "../component/AddTask";
 import TaskList from "../component/TaskList";
 import taskApi from "../api/taskApi";
 import { toast } from "react-toastify";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const [, setTasks] = useRecoilState(tasksState);
+  const [isFetching, setIsFetching] = useState(true);
 
   const [cookies] = useCookies("token");
 
@@ -28,32 +30,39 @@ const Dashboard = () => {
         } else {
           setTasks(result);
         }
+        setIsFetching(false);
       } catch (err) {
         toast.error(err);
       }
     })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Layout>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <AssignmentIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Tasks
-        </Typography>
-        <AddTask />
-        <TaskList />
-      </Box>
+      {isFetching ? (
+        <Box sx={{ display: "flex", justifyContent: "center", marginTop: 8 }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <AssignmentIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Tasks
+          </Typography>
+          <AddTask />
+          <TaskList />
+        </Box>
+      )}
     </Layout>
   );
 };
